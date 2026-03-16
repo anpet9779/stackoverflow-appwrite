8b0b2839276dd60f2e9e6ac93fdb96f90e16bee0
Parag Petkar <anpet9779@gmail.com>
2026-03-16 22:27:35 +0530

    Initial commit

---
+++ src/env.ts
@@ -0,0 +1,7 @@
+const env = {
+  appwrite: {
+    projectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
+    endpoint: String(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT),
+    key: String(process.env.NEXT_PUBLIC_APPWRITE_KEY),
+  },
+};
